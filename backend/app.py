dcfbaa8414d396d58d1ebc15c9c89c273fc7712d
jensen zhang <jensenzhang@jensens-MacBook-Pro.local>
2023-10-14 13:21:36 -0400

    connection to mongodb

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ import os
 import json
 from dotenv import load_dotenv
 
-from flask import Flask, request, json
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 from langchain.chat_models import ChatOpenAI
@@ -12,6 +12,8 @@ from response import get_summary
 
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+import certifi
 import re
 
 load_dotenv()
@@ -20,23 +22,15 @@ app = Flask(__name__)
 CORS(app) 
 chat_model = ChatOpenAI(openai_api_key=os.environ.get('api-key'),
                         model_name='gpt-3.5-turbo-16k',
-                        temperature = 0.5,
+                        temperature = 0,
                         max_tokens=8000)
 
-@app.route('/summarize-policy', methods=['POST'])
-def summarize_policy():
-    link_object = request.get_json()
-    print(link_object['link'])
-    privacy_text = fetchPrivacyPolicy(link_object['link'])
-    print(privacy_text)
-    print(count_tokens(privacy_text))
-    
-    # return "hi"
-    if (count_tokens(privacy_text) > 15000):
-        privacy_text = privacy_text[:40000]
-    return get_summary(chat_model=chat_model, privacy_policy=privacy_text)
+ca = certifi.where()
+client = MongoClient(os.environ.get('mongo-uri'), tlsCAFile=ca)
+db = client['Contractly']
+collection = db['summaries']
 
-def fetchPrivacyPolicy(link):
+def fetch_privacy_policy(link):
     try:
         response = requests.get(link)
         
@@ -60,6 +54,34 @@ def count_tokens(text):
     tokens = re.findall(r'\b\w+\b', text)
     return len(tokens) * 2
 
+@app.route('/summarize-policy', methods=['POST'])
+def summarize_policy():
+    link_object = request.get_json()
+    print(link_object['link'])
+
+    summary = collection.find_one({'base_domain': link_object['baseDomain']})
+
+    if (summary is None):
+        privacy_text = fetch_privacy_policy(link_object['link'])
+        print(privacy_text)
+        print(count_tokens(privacy_text))
+        if (count_tokens(privacy_text) > 15000):
+            privacy_text = privacy_text[:40000]
+        statement =  get_summary(chat_model=chat_model, privacy_policy=privacy_text)
+        summary_object = {
+            'base_domain': link_object['baseDomain'],
+            'summary': statement
+        }
+        collection.insert_one(summary_object)
+        return statement
+    else:
+        return summary['summary'];
+    
+    # collection.insert_one(summary_object)
+    print('##')
+    print(collection.find_one({'base_domain': link_object['baseDomain']}))
+    
+    return 'hi'
 # TODO: 
 # - implement getting privacy policy html within python backend
 # - localstorage visited before
