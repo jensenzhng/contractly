518f9c8bcfde46e7d2616f86c7513682313c2f5e
jensen zhang <jensenzhang@lawn-128-61-7-211.lawn.gatech.edu>
2023-10-14 23:07:46 -0400

    extension running

--- backend/app.py
+++ backend/app.py
@@ -107,4 +107,4 @@ def summarize_policy():
 #     return jsonify(langchain_summary)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
